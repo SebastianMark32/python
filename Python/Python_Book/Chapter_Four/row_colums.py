2c3ad151a0a995326e505f98d3f984b2e64c6d66
Sebastian Mark <smarkgolfer@gmail.com>
2023-08-22 10:02:55 -0400

    Spacing update

--- Python/Python_Book/Chapter_Four/row_colums.py
+++ Python/Python_Book/Chapter_Four/row_colums.py
@@ -4,9 +4,7 @@
 rows = int(input("How many rows would you like?:"))
 columns = int(input("How many colums would you like?:"))
 
-
 # Nested loop to build rows and colums
-
 for r in range(rows):
     for c in range(columns):
         print("*", end='')
